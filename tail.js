8f12892ce6d64e6e6e8de6e90ad0dac67836a110
Phil <philippe.teigne@gmail.com>
2023-10-17 22:15:44 -0400

    added functionality to tail function

---
+++ tail.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😎😎 Assertion Passed: ${actual} === ${expected} 😎😎`);
+  }
+  if (actual !== expected) {
+    console.log(`💔💔 Assertion Failed: ${actual} !== ${expected} 💔💔`);
+  }
+  
+};
+
+const tail = function(arrTailItems) { //  return everything other than the head of an array
+  let returnArr = [];
+  for (let i = 1; i < arrTailItems.length; i++) {
+    returnArr.push(arrTailItems[i]);
+  }
+  return returnArr;
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+assertEqual(tail([1, 2, 3, 4]).length, 3);
+assertEqual(tail([1, 2, 3, 4])[0], 2);
+assertEqual(tail([1, 2, 3, 4])[1], 3);
+assertEqual(tail([1, 2, 3, 4])[2], 4);
+
+assertEqual(tail([1]).length, 0);
+assertEqual(tail([1])[0], undefined);
+
+assertEqual(tail([]).length, 0);
+assertEqual(tail([])[0], undefined);
