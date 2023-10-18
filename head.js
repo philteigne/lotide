5d9cbf09e07ac2438fda50ba4cc3004101be4839
Phil <philippe.teigne@gmail.com>
2023-10-17 22:08:23 -0400

    changed function to function expression

--- head.js
+++ head.js
@@ -16,8 +16,3 @@ assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([5]), 5);
 assertEqual(head([]), 5);
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
