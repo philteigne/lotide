2e5b6466b36bac1c13d6921584d51de7b4e0e142
Phil <philippe.teigne@gmail.com>
2023-10-17 22:06:35 -0400

    changed function to function expression

--- head.js
+++ head.js
@@ -8,9 +8,14 @@ const assertEqual = function(actual, expected) {
   
 };
 
-function head(arrFirstItem) {
-  return arrFirstItem[0]
+const head = function(arrFirstItem) {
+  return arrFirstItem[0];
 };
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5]), 5);
+assertEqual(head([]), 5);
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
