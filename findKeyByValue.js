65f5faed660d90d7efaf581da8038723aeaf7e2d
Phil <philippe.teigne@gmail.com>
2023-10-21 18:21:05 -0400

    add findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😎😎 Assertion Passed: ${actual} === ${expected} 😎😎`);
+  }
+  if (actual !== expected) {
+    console.log(`💔💔 Assertion Failed: ${actual} !== ${expected} 💔💔`);
+  }
+  
+};
+
+//  look through searchObject and return the first key that contains findValue
+const findKeyByValue = function(searchObject, findValue) {
+
+  // const keyArray = Object.keys(searchObject);
+
+  // for (let i of keyArray) {
+  //   if (searchObject[i] === findValue) {
+  //     return i;
+  //   }
+  // }
+
+  for (let i in searchObject) {
+    if (searchObject[i] === findValue) {
+      return i;
+    }
+  }
+
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+
