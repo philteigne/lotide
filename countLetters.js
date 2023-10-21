8722224858a595e5592f8c36dd7fc274e183c0b3
Phil <philippe.teigne@gmail.com>
2023-10-21 15:42:28 -0400

    add countLetters.js to count letter instances in a sentence

---
+++ countLetters.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😎😎 Assertion Passed: ${actual} === ${expected} 😎😎`);
+  }
+  if (actual !== expected) {
+    console.log(`💔💔 Assertion Failed: ${actual} !== ${expected} 💔💔`);
+  }
+  
+};
+
+// Given a sentence, count all letters in the sentence and return an object containing each letter's count.
+const countLetters = function(sentenceToCount) {
+  //  convert sentence into lowercase for consistent comparing
+  sentenceToCount = sentenceToCount.toLowerCase();
+
+  const resultObj = {};
+
+  for (let i of sentenceToCount) {
+    if (i === ' ') {  // do not count spaces
+      continue;
+    }
+
+    if (!resultObj[i]) {  //  if property does not exist in resultObj create one
+      resultObj[i] = 0;
+    }
+
+    resultObj[i] ++; //  Count instance of letter
+
+  }
+
+  return resultObj;
+
+};
+
+const countLettersResult = countLetters('LHL');
+
+assertEqual(countLettersResult['l'], 2);
+assertEqual(countLettersResult['h'], 1);
