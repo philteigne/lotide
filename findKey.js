0103e2112a4c2ed2ce306fef8ed8e3eff46ec9f3
Phil <philippe.teigne@gmail.com>
2023-10-22 19:52:46 -0400

    add findKey that finds the first key in a given object that satisfies a given property

---
+++ findKey.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😎😎 Assertion Passed: ${actual} === ${expected} 😎😎`);
+  }
+  if (actual !== expected) {
+    console.log(`💔💔 Assertion Failed: ${actual} !== ${expected} 💔💔`);
+  }
+  
+};
+
+const findKey = function (object, callback) {
+
+  let keys = Object.keys(object);
+
+  for (let i of keys) {
+    if (callback(object[i])) {
+      return i;
+    }
+  }
+
+};
+
+let result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+let result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3);
+
+assertEqual(result1, "noma");
+assertEqual(result2, "Akaleri");
