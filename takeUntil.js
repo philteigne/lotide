f506d300f7537cf60d9548e03080f7c601be1589
Phil <philippe.teigne@gmail.com>
2023-10-22 19:37:58 -0400

    add takeUntil that creates an array containing given array elements until given condition evaluates to true

---
+++ takeUntil.js
@@ -0,0 +1,51 @@
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {  //  If arrays are different sizes, return false
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) { // Iterate over both arrays, and compare elements
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+
+  let equalArrays = eqArrays(arr1, arr2);
+
+  if (equalArrays) {
+    console.log(`😎😎 Assertion Passed: ${arr1} === ${arr2} 😎😎`);
+  }
+
+  if (!equalArrays) {
+    console.log(`💔💔 Assertion Failed: ${arr1} !== ${arr2} 💔💔`);
+  }
+
+};
+
+const takeUntil = function(array, callback) {
+
+  let results = [];
+
+  for (let i of array) {
+    if (callback(i)) {
+      return results;
+    }
+
+    results.push(i);
+  }
+
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
