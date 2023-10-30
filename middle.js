6d5e3847d42685c007e500e4e00f21b8264ecbd9
Phil <philippe.teigne@gmail.com>
2023-10-30 13:37:27 -0400

    refactored middle and moved test cases to /test

--- middle.js
+++ middle.js
@@ -1,7 +1,3 @@
-const eqArrays = require('./eqArrays');
-
-const assertArraysEqual = require('./assertArraysEqual');
-
 const middle = function(arrayToMiddle) {
 
   let arrayLength = arrayToMiddle.length;
@@ -29,10 +25,3 @@ const middle = function(arrayToMiddle) {
 };
 
 module.exports = middle;
-
-// assertArraysEqual(middle([1]), []);
-// assertArraysEqual(middle([1, 2]), []);
-// assertArraysEqual(middle([1, 2, 3]), [2]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
