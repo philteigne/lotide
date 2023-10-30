92081e42872e4c122b6241161ecf96b422d41480
Phil <philippe.teigne@gmail.com>
2023-10-30 13:14:58 -0400

    refactor to simplyify code and remove unnecessary imports

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,7 +1,6 @@
 const assertEqual = require('../assertEqual');
-const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
+assertEqual(5 * 5, 25);
+assertEqual(1 > 0, true);
+assertEqual(typeof 'Hello', 'string');
+assertEqual(5 - 5, 0);
