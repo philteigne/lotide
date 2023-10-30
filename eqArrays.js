7aac21570af20b8ae0cb0f2e09472ac4405d915d
Phil <philippe.teigne@gmail.com>
2023-10-30 13:26:29 -0400

    refactored eqArrays, moved test cases to /test

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 const eqArrays = function(arr1, arr2) {
 
   if (arr1.length !== arr2.length) {  //  If arrays are different sizes, return false
@@ -27,15 +25,3 @@ const eqArrays = function(arr1, arr2) {
 };
 
 module.exports = eqArrays;
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-// assertEqual(eqArrays([], []), true);
-
-// console.log('-----');
-
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
