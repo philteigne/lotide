28fd0cec9327d6d93fac69476f47fb601e54fdfb
Phil <philippe.teigne@gmail.com>
2023-10-30 14:14:41 -0400

    fix typos in npm test messages

--- test/middleTest.js
+++ test/middleTest.js
@@ -14,7 +14,7 @@ describe('#middle', () => {
   it('returns [3] for [1, 2, 3, 4, 5]', () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
   });
-  it('returns [1, 2, 3, 4] for [2, 3]', () => {
+  it('returns [2, 3] for [1, 2, 3, 4]', () => {
     assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
   it('returns [3, 4] for [1, 2, 3, 4, 5, 6]', () => {
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,7 @@
 const assert = require('chai').assert;
 const tail = require('../tail');
 
-describe("#head", () => {
+describe("#tail", () => {
   it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
     assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
   });
